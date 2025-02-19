4c9494bb83d82fdc25e707ea9ee3fb40772a197a
wpulier <your.github.email@example.com>
2025-02-18 17:07:13 -0800

    fix: implement proper vercel output directory structure

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import express, { type Request, Response, NextFunction } from "express";
-import { registerRoutes } from "./routes.ts";
+import { registerRoutes } from "./routes.js";
 import { setupVite, serveStatic, log } from "./vite";
 import { config } from "./config";
 
